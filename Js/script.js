802ad6add7faa200b6b96353a12b4967364a0446
Bruno Candido dos Santos <111137962+BrunoSantos37@users.noreply.github.com>
2023-12-01 11:42:54 -0300

    Colocando cor no fundo de acordo com horário

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,7 @@
 function carregar(){
 let msg = document.querySelector('.msg');
 let imagem = document.querySelector('.img');
+const body = document.querySelector('body');
 
 let data = new Date();
 let hora = data.getHours();
@@ -9,6 +10,8 @@ msg.innerHTML = `Agora são ${hora} horas!`;
 
     if (hora >= 6 && hora < 12 ){
         imagem.src = '/imagens/fotomanha.jpg';
+        body.style.background = rgb(255,248,0);
+        body.style.background = "linear-gradient(326deg, rgba(255,248,0,1) 11%, rgba(0,194,255,1) 100%)";
     }else if (hora >= 12 && hora <= 18){
         imagem.src = '/imagens/fototarde.jpg';
     }else {
